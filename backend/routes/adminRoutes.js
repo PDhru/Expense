f1466dcc8a340237bd9afc5feb82cc27134355b0
[PDhru] <[dhrutip2005@gmail.com]>
2024-12-15 21:26:14 +0530

    testing changes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const { protect, admin } = require('../middlewares/authMiddleware');
-const User = require('../models/userModel');
+const express = require("express");
+const { protect, admin } = require("../middlewares/authMiddleware");
+const User = require("../models/userModel");
 // const { getAllUsersWithExpenses } = require('../controllers/adminController');
 const router = express.Router();
 
-
-// Example admin route: Get all users (only admins can access this)
-router.get('/users', protect, admin, async (req, res) => {
+router.get("/users", protect, admin, async (req, res) => {
   const users = await User.find({});
-  console.log(users)
+  console.log(users);
   res.json(users);
 });
 
-// router.get('/users-expenses', protect, admin, getAllUsersWithExpenses);
-
 module.exports = router;
-
